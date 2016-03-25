6e49eba176f3440956bea22eebe7c83934cc37cf
Joe Case <jcase146@gmail.com>
2016-03-25 12:40:35 -0400

    Add flash messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(username: 'joe', email: 'joe@joe.com', password: 'pw', description: 'I am a ruby ninja and OO disciple who likes red wine and ASP.net')
+User.create(username: 'ryan', email: 'ryan@ryan.com', password: 'pw', description: 'I am a regular person with hobbies and stuff.')
+User.create(username: 'peter', email: 'peter@peter.com', password: 'pw', description: 'I am Batman.')
