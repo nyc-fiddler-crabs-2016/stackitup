01c8df3e5eb8acae297ebe75c1af7934705ad698
Ryan F. Salerno <ryan@salerno.co>
2016-03-26 17:57:03 -0400

    ajax answer editing

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -11,23 +11,8 @@ $( document ).ready(function() {
   $('.answer-edit-form').on('ajax:success', function(e, data, status, xhr) {
     $('#edit-answer').hide();
     $('#answer').show();
-    debugger;
-    $('#answer').html('<p>J Case</p>');
+    $('#answer').html(xhr.responseText);
 
   });
 
-  // $('.answer-edit-form').on('ajax:success', (e, data, status, xhr)
-  //   $('#answer').append xhr.responseText).on 'ajax:error', (e, xhr, status, error)
-  // $('#answer').append '<p>error</p>';
-});
-
-
-
-
-$(document).ready(function() {
-  return $("#new_article").on("ajax:success", function(e, data, status, xhr) {
-    return $("#new_article").append(xhr.responseText);
-  }).on("ajax:error", function(e, xhr, status, error) {
-    return $("#new_article").append("<p>ERROR</p>");
-  });
 });
