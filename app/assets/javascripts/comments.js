e562e7fcac1ec287fd15460e7b466be05d5ac2f2
Petey101 <pleong101@live.com>
2016-03-27 20:23:24 -0400

    Comment out AJAX for presentation

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$( document ).ready(function() {
-  $('.add-comment-link').on('click', function(event){
-    event.preventDefault();
-    console.log("blocked");
-    var that = this
-    $.ajax({
-    	type: "GET",
-    	url: $(this).attr("href"),
-    }).done(function(response){
-    	$(that).replaceWith(response)
-    })
-  });
+// $( document ).ready(function() {
+//   $('.add-comment-link').on('click', function(event){
+//     event.preventDefault();
+//     console.log("blocked");
+//     var that = this
+//     $.ajax({
+//     	type: "GET",
+//     	url: $(this).attr("href"),
+//     }).done(function(response){
+//     	$(that).replaceWith(response)
+//     })
+//   });
 
-});
+// });
